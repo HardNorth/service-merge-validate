ec69c6574ba269c0aec0d5b96f9082236e43848e
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-07-08 10:43:55 +0300

    Adding service structure

---
+++ src/main/java/net/hardnorth/github/merge/api/MergeValidateController.java
@@ -0,0 +1,8 @@
+package net.hardnorth.github.merge.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("merge-validate")
+public class MergeValidateController {
+
+}
---
+++ src/main/java/net/hardnorth/github/merge/context/MergeValidateContext.java
@@ -0,0 +1,11 @@
+package net.hardnorth.github.merge.context;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:merge-validate.properties")
+public class MergeValidateContext {
+
+}
