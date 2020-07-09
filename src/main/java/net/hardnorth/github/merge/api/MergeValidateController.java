51bef79e7fb9f3d9f9e89ca25d89aff88ada4fbd
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-07-09 10:36:30 +0300

    Adding first API

--- src/main/java/net/hardnorth/github/merge/api/MergeValidateController.java
+++ src/main/java/net/hardnorth/github/merge/api/MergeValidateController.java
@@ -1,8 +1,37 @@
 package net.hardnorth.github.merge.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("merge-validate")
 public class MergeValidateController {
+
+	@GetMapping("healthcheck")
+	public ResponseEntity<String> healthCheck()
+	{
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set(HttpHeaders.CONTENT_TYPE, MimeTypeUtils.TEXT_PLAIN_VALUE);
+
+		return ResponseEntity.ok()
+				.headers(responseHeaders)
+				.body(this.getClass().getAnnotation(RestController.class).value() + ": OK");
+	}
+
+	@PostMapping("integration")
+	public void createIntegration(@RequestBody String repositoryUrl) {
+
+	}
+
+	@DeleteMapping("integration")
+	public void deleteIntegration(@RequestBody String repositoryUrl) {
+
+	}
+
+	@PutMapping("merge/{from}/{to}")
+	public void merge(@PathVariable String from, @PathVariable String to) {
+
+	}
 
 }
