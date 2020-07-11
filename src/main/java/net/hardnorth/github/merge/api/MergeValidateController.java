48b5914ce2d8614248a42a4da26f856e4a799525
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-07-11 13:43:59 +0300

    Configuring AppEngine server

--- src/main/java/net/hardnorth/github/merge/api/MergeValidateController.java
+++ src/main/java/net/hardnorth/github/merge/api/MergeValidateController.java
@@ -19,6 +19,16 @@ public class MergeValidateController {
 				.body(this.getClass().getAnnotation(RestController.class).value() + ": OK");
 	}
 
+	@PostMapping()
+	public void ping(@RequestBody String body) {
+		System.out.println(body);
+	}
+
+	@PostMapping("authorize")
+	public void authorize(@RequestBody String body) {
+
+	}
+
 	@PostMapping("integration")
 	public void createIntegration(@RequestBody String repositoryUrl) {
 
