ff3180b7bf3da8e886668150266dcc213e4f40cf
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-12-27 19:11:56 +0300

    Controller update

--- src/main/java/net/hardnorth/github/merge/api/MergeValidateController.java
+++ src/main/java/net/hardnorth/github/merge/api/MergeValidateController.java
@@ -5,8 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController("merge-validate")
 public class MergeValidateController {
+	private static final List<String> scopes = Arrays.asList("repo", "user:email");
+	private static final String clientId = "Iv1.556c0a74a6200c54";
+	private static final String redirectUriPattern = "https://merge.hardnorth.net/result/%s";
 
 	@GetMapping("healthcheck")
 	public ResponseEntity<String> healthCheck()
@@ -19,28 +25,25 @@ public class MergeValidateController {
 				.body(this.getClass().getAnnotation(RestController.class).value() + ": OK");
 	}
 
-	@PostMapping()
+	@PostMapping
 	public void ping(@RequestBody String body) {
 		System.out.println(body);
 	}
 
-	@PostMapping("authorize")
-	public void authorize(@RequestBody String body) {
-
-	}
-
 	@PostMapping("integration")
-	public ResponseEntity<Void> createIntegration(@RequestBody String repositoryUrl) {
-		return null; // return URL
+	public ResponseEntity<Void> createIntegration(@RequestParam String repositoryUrl) {
+		// see: https://docs.github.com/en/free-pro-team@latest/developers/apps/authorizing-oauth-apps
+		return null; // return redirect URL in headers, authUuid
 	}
 
-	@GetMapping("return")
-	public ResponseEntity<Void> integrationResult(@RequestParam String code) {
+	@GetMapping("result/{authUuid}")
+	public ResponseEntity<String> integrationResult(@PathVariable("authUuid") String authUuid, @RequestParam("state") String state,
+			@RequestParam("code") String code) {
 		return null; // Repo UUID
 	}
 
 	@PutMapping("merge/{uuid}/{from}/{to}")
-	public void merge(@PathVariable String uuid, @PathVariable String from, @PathVariable String to) {
+	public void merge(@PathVariable("uuid") String uuid, @PathVariable("from") String from, @PathVariable("to") String to) {
 
 	}
 
