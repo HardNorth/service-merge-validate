2cc6e1b0ce432d3377ea79a18a42ffed653585c5
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-12-28 08:39:22 +0300

    Test fixes

--- src/test/java/net/hardnorth/github/merge/MergeValidateApplicationTests.java
+++ src/test/java/net/hardnorth/github/merge/MergeValidateApplicationTests.java
@@ -1,5 +1,9 @@
 package net.hardnorth.github.merge;
 
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +13,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MergeValidateApplicationTests
 {
+    private final LocalServiceTestHelper helper =
+            new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+    @Before
+    public void setUp() {
+        helper.setUp();
+    }
+
+    @After
+    public void tearDown() {
+        helper.tearDown();
+    }
 
     @Test
     public void contextLoads()
