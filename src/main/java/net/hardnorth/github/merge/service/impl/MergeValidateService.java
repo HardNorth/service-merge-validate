5b1f9f8a823814851d59c7ce00b27cd4205b4678
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-02-24 09:56:52 +0300

    MergeValidateService WIP

--- src/main/java/net/hardnorth/github/merge/service/impl/MergeValidateService.java
+++ src/main/java/net/hardnorth/github/merge/service/impl/MergeValidateService.java
@@ -7,23 +7,19 @@ import net.hardnorth.github.merge.exception.HttpException;
 import net.hardnorth.github.merge.model.Charset;
 import net.hardnorth.github.merge.service.GithubApiClient;
 import net.hardnorth.github.merge.service.MergeValidate;
+import net.hardnorth.github.merge.utils.ValidationPattern;
 import net.hardnorth.github.merge.utils.WebClientCommon;
 import org.apache.http.HttpStatus;
 import retrofit2.Response;
 
 import javax.annotation.Nonnull;
 import java.util.Base64;
-import java.util.logging.Logger;
+import java.util.Collections;
+import java.util.List;
 
 public class MergeValidateService implements MergeValidate {
-
-    private static final Logger LOGGER = Logger.getLogger(MergeValidateService.class.getSimpleName());
-
     private static final RuntimeException UNABLE_TO_GET_CONFIGURATION_EXCEPTION_INVALID_RESPONSE
             = new HttpException("Unable to get merge configuration for target branch: invalid response", HttpStatus.SC_FAILED_DEPENDENCY);
-
-    private static final RuntimeException UNABLE_TO_GET_CONFIGURATION_EXCEPTION_LIMIT_EXCEED
-            = new HttpException("Unable to get merge configuration for target branch: scan limit exceed", HttpStatus.SC_REQUEST_TOO_LONG);
 
     private static final RuntimeException UNABLE_TO_GET_CONFIGURATION_EXCEPTION_NO_FILE
             = new HttpException("Unable to get merge configuration for target branch: no configuration file found", HttpStatus.SC_BAD_REQUEST);
@@ -109,13 +105,18 @@ public class MergeValidateService implements MergeValidate {
             throw UNABLE_TO_GET_CONFIGURATION_RESPONSE_IS_NOT_JSON;
         }
 
-        return new String(Base64.getDecoder().decode(file.getAsJsonPrimitive(CONTENT_FIELD).getAsString()), charset);
+        return new String(Base64.getDecoder().decode(file.getAsJsonPrimitive(CONTENT_FIELD).getAsString()
+                .replace("\n", "").replace("\r", "")), charset);
+    }
+
+    private List<String> getChangedFiles(String authHeader, String repo, String from, String to) {
+        return Collections.emptyList();
     }
 
     @Override
     public void merge(String authHeader, String repo, String from, String to) {
         String mergeFileContent = getMergeFileContent(authHeader, repo, to);
-        LOGGER.finer("Got merge file: " + mergeFileContent);
+        ValidationPattern pattern = ValidationPattern.parse(mergeFileContent);
 
     }
 }
--- src/test/java/net/hardnorth/github/merge/service/MergeValidateServiceTest.java
+++ src/test/java/net/hardnorth/github/merge/service/MergeValidateServiceTest.java
@@ -7,16 +7,19 @@ import net.hardnorth.github.merge.model.Charset;
 import net.hardnorth.github.merge.service.impl.MergeValidateService;
 import net.hardnorth.github.merge.utils.IoUtils;
 import okhttp3.Headers;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.http.HttpStatus;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import retrofit2.Call;
 import retrofit2.Response;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -26,31 +29,58 @@ import static org.mockito.Mockito.when;
 
 public class MergeValidateServiceTest {
 
+    private static final String MERGE_FILE_NAME = ".merge-validate";
+
     private static final Gson GSON = new Gson();
 
     private final GithubApiClient github = mock(GithubApiClient.class);
 
 
     private final MergeValidate service =
-            new MergeValidateService(github, ".merge-validate", new Charset(StandardCharsets.UTF_8), 512000);
+            new MergeValidateService(github, MERGE_FILE_NAME, new Charset(StandardCharsets.UTF_8), 512000);
 
-    @ParameterizedTest
-    @ValueSource(strings = {"github/file_list_no_merge_file.txt", "github/file_list_no_merge_file_large.txt"})
+
+    public static Iterable<Object[]> invalidResponses() {
+        return Arrays.asList(new Object[]{"github/file_list_no_merge_file.json", HttpException.class, HttpStatus.SC_BAD_REQUEST, "no configuration file found"},
+                new Object[]{"github/file_list_no_merge_file_large.json", HttpException.class, HttpStatus.SC_BAD_REQUEST, "no configuration file found"},
+                new Object[]{"github/file_list_merge_file_too_large.json", HttpException.class, HttpStatus.SC_REQUEST_TOO_LONG, "file size limit exceed"});
+    }
+
+
     @SuppressWarnings("unchecked")
-    public void verify_github_no_merge_file_response(String file) throws IOException {
+    private Pair<Call<JsonElement>, Response<JsonElement>> mockContentCall(String path, JsonElement responseBody) throws IOException {
         Call<JsonElement> call = mock(Call.class);
         Response<JsonElement> response = mock(Response.class);
         when(call.execute()).thenReturn(response);
         when(response.isSuccessful()).thenReturn(Boolean.TRUE);
-        when(github.getContent(anyString(), anyString(), anyString(), eq("dest"))).thenReturn(call);
-        when(response.body())
-                .thenReturn(GSON.fromJson(IoUtils.readInputStreamToString(getClass().getClassLoader().getResourceAsStream(file)), JsonElement.class));
+        when(github.getContent(anyString(), anyString(), eq(path), eq("dest"))).thenReturn(call);
+        when(response.body()).thenReturn(responseBody);
         when(response.headers()).thenReturn(Headers.of());
-
-
-        HttpException result = Assertions.assertThrows(HttpException.class, () -> service.merge("auth", "HardNorth/test", "source", "dest"));
-        assertThat(result.getMessage(), Matchers.endsWith("no configuration file found"));
+        return Pair.of(call, response);
     }
 
+    @ParameterizedTest
+    @MethodSource("invalidResponses")
+    public <T extends HttpException> void verify_github_bad_merge_file_responses(String file, Class<T> exception, int status, String message) throws IOException {
+        mockContentCall("", GSON.fromJson(IoUtils.readInputStreamToString(getClass().getClassLoader().getResourceAsStream(file)), JsonElement.class));
+        T result = Assertions.assertThrows(exception, () -> service.merge("auth", "HardNorth/test", "source", "dest"));
+        assertThat(result.getCode(), Matchers.equalTo(status));
+        assertThat(result.getMessage(), Matchers.endsWith(message));
+    }
+
+    @Test
+    public void verify_github_merge_success() throws IOException {
+        mockContentCall("", GSON.fromJson(
+                IoUtils.readInputStreamToString(getClass().getClassLoader().getResourceAsStream("github/file_list_merge_file.json")),
+                JsonElement.class
+        ));
+
+        mockContentCall(MERGE_FILE_NAME, GSON.fromJson(
+                IoUtils.readInputStreamToString(getClass().getClassLoader().getResourceAsStream("github/merge_file_default.json")),
+                JsonElement.class
+        ));
+
+        service.merge("auth", "HardNorth/test", "source", "dest");
+    }
 
 }
